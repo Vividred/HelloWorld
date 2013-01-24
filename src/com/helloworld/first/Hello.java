5651bd76fbd477eed66f86ffb53921944401722c
Vividred <Vividred.impact@gmail.com>
2013-01-24 17:51:28 +0800

    test2

---
+++ src/com/helloworld/first/Hello.java
@@ -0,0 +1,17 @@
+package com.helloworld.first;
+
+public class Hello
+{
+
+	/**
+	 * 原来写这篇代码的时候，只有我和上帝知道它是干嘛的
+	 * 现在只有上帝知道了
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
